45f8ae70cb31c5de1ac33b14d441193bbd34a5ec
Monica  Karasek <mlkarasek@gmail.com>
2018-09-09 07:29:41 +0000

    automatically backed up by learn

--- db/migrate/20180909072929_add_column_product.rb
+++ db/migrate/20180909072929_add_column_product.rb
@@ -1,4 +1,6 @@
-class AddColumnProduct < ActiveRecord::Migration
+class AddColumnProductToProducts < ActiveRecord::Migration
   def change
+    add_column :products, :product, :string
+    remove_column :products, :name, :string
   end
 end
